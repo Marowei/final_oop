876e7f37edea528096e0233a5c577ced6fb93f34
Marowei <m10702136@gapps.ntust.edu.tw>
2019-01-01 21:15:40 +0800

    debug teacher student graderstudent

--- finalOOP_pro1/finalOOP_pro1/project1_starter.h
+++ finalOOP_pro1/finalOOP_pro1/project1_starter.h
@@ -10,140 +10,62 @@ using namespace std;
 
 enum StudentStatus{FullTime, PartTime, Exchange, sUnknown};
 
-static const string statusLabels[] = {"full-time","part-time",
-                                      "exchange","unknown"};
-
 
 enum Department{Accounting, Business, Engineering, Mathematics,
                 Physics, Arts, Chemistry, dUnknown};
 
-static const string departmentNames[] = {"Accounting",
-                                         "Business",
-                                         "Engineering",
-                                         "Mathematics",
-                                         "Physics",
-                                         "Arts",
-                                         "Chemistry",
-                                         "Unknown"};
-
 enum Rank {Instructor,GradTeachingAsst,AsstProf,
            AssocProf,Professor,ResScientist,Dean,rUnknown};
 
-
-static string rankNames[] = {"Instructor",
-                             "GradTeachingAsst",
-                             "AsstProf",
-                             "AssocProf",
-                             "Professor",
-                             "ResScientist",
-                             "Dean"
-                             "Unknown"};
-
-
-
-
-
+class Person;
+class Student;
 ///////////////   utility class Date   ////////////////
-
 
 class Date {
 public:
-  int d, m, y;
-  static Date today;
-  Date( int dd=0, int mm=0, int yy=0 )
-  {
-	checkDate(dd,mm,yy);
-    dd ? d = dd : today.d;
-    mm ? m = mm : today.m;
-    yy ? y = yy : today.y;
-  }
-  void checkDate(int &d, int &m, int &y)
-  {
-	  if (d > 31 || d < 0)
-	  {
-		  cout << "set day is error." << endl << "auto set to " << today.d << endl;
-		  d = today.d;
-	  }
-	  if (m > 12 || m < 0)
-	  {
-		  cout << "set month is error." << endl << "auto set to " << today.m << endl;
-		  m = today.m;
-	  }
-	  if (y > 2019 || y < 0)
-	  {
-		  cout << "set day is error." << endl << "auto set to " << today.y << endl;
-		  y = today.y;
-	  }
-  }
-  friend ostream& operator<<( ostream&, Date );
+	int d, m, y;
+	static Date today;
+	Date(int dd = 0, int mm = 0, int yy = 0);
+	void checkDate(int &d, int &m, int &y);
+	friend ostream& operator<<( ostream&, Date );
 };
-
-ostream& operator<<( ostream& os, Date dt )
-{
-  os << dt.m << "-" << dt.d << "-" << dt.y << endl;
-  return os;
-}
-
-
-Date Date::today( 1, 1, 2000 );
-
-
-
 /////////////     utility class Course      ///////////////
 
-
+constexpr int MaxStudentForCourses = 50;
 class Course{
 
   string courseName;
   long   courseId;
+  Student* courseOwner[MaxStudentForCourses];
+  int ownerCounter;
 
 public:
 
-	Course(): courseName("null"), courseId(0){
-	};                                  
+	Course();                                  
 	// no-arg constructor
-	Course(const string, long):courseName(courseName), courseId(courseId){
-	};              
+	Course(const string, long);              
 	// constructor
-	Course(const Course& other) {
-		courseName = other.courseName;
-		courseId = other.courseId;
-	};                   
+	Course(const Course& other);                   
 	// copy constructor
-	Course& operator=(const Course& other) {
-		if (this!=&other) {
-			courseName = other.courseName;
-			courseId = other.courseId;
-		}
-		return *this;
-	};  
+	Course& operator=(const Course& other);
 	// copy assignment op  
-	~Course() {
-	};                                 
+	~Course();
 	// destructor
+	bool addOwnerStudent(Student& student);
+	bool removeOwnerStudent(Student& student);
+	void removeAllOwnerStudent();
 
+	void listStudentsRegisteredFor();
   // comparison operators:
-  bool operator==( const Course& );
-  bool operator!=( const Course& );
+	bool operator==(const Course& other) const;
+	bool operator!=(const Course& other) const;
+	
+	string getCourseName() const;
+	void setCourseName(const string CourseName);
+	long getCourseId() const;
+	void setCourseId(long CourseId);
 
-  string getCourseName() const {
-	  return courseName;
-  };
-  void setCourseName(const string CourseName) {
-	  courseName = CourseName;
-  };
-  long getCourseId() const {
-	  return courseId;
-  };
-  void setCourseId(long CourseId) {
-	  courseId = CourseId;
-  };
-
-  friend ostream& operator<<( ostream& os, Course aCourse )
-  {
-    os << aCourse.courseName << "  "  << aCourse.courseId << endl;
-    return os;
-  }
+	friend ostream& operator<<(ostream& os, Course aCourse);
 };
 
 
@@ -158,63 +80,28 @@ class Person{
   
 public:
     
-  Person():name("Person"), address("No.43,Section 4, Keelung Rd, Da'an District, Taipei City,106"), ssn(123456789), birthdate(Date::today) 
-  {
-  }
-  // constructor:
-  Person(const string theName, unsigned long theSSN, Date theBday, const string theAddress):name(theName), address(theAddress), ssn(theSSN), birthdate(theBday) {
-  }
-  // copy constructor:
-  Person(const Person& obj) {
-	  name = obj.name;
-	  address = obj.address;
-	  ssn = obj.ssn;
-	  birthdate = obj.birthdate;
-  }
+	Person();
+	// constructor:
+	Person(const string theName, unsigned long theSSN, Date theBday, const string theAddress);
+	// copy constructor:
+	Person(const Person& obj);
   
-  // copy assignment operator:
-  Person& operator=(const Person& obj) {
-	  name = obj.name;
-	  address = obj.address;
-	  ssn = obj.ssn;
-	  birthdate = obj.birthdate;
-	  return *this;
-  }
-  // destructor:
-  ~Person() {
-  
-  }
-  void setName(const string theName) {
-	  name = theName;
-  }
-  string getName() const {
-	  return name;
-  }
-  void setAddress(const string theAddress) {
-	  address = theAddress;
-  }
-  string getAddress() const {
-	  return address;
-  }
-  Date getBirthDate() const {
-	  return birthdate;
-  }
-  unsigned long getIdentification() const {
-	  return ssn;
-  }
-  void setIdentification(unsigned long newID) {
-	  ssn = newID;
-  }
-  virtual void print() const {
-	  cout << "name = " << name << endl
-		  << "address = " << address << endl
-		  << "Identification = " << ssn << endl
-		  << "BirthDate = " << birthdate << endl;
-  }
+	// copy assignment operator:
+	Person& operator=(const Person& obj);
+	bool operator==(const Person& obj) const;
+	bool operator!=(const Person& obj) const;
+	// destructor:
+	~Person();
+	void setName(const string theName);
+	string getName() const;
+	void setAddress(const string theAddress);
+	string getAddress() const;
+	Date getBirthDate() const;
+	unsigned long getIdentification() const;
+	void setIdentification(unsigned long newID);
+	virtual void print() const;
 };
-
-
-const int MaxCoursesForStudent = 4;
+constexpr int MaxCoursesForStudent = 4;
 
 
 ///////////////// derived class Student //////////////
@@ -224,13 +111,12 @@ class Student : virtual public Person {
 
   StudentStatus status;
   Department    department;
-  Course*       enrolled[ MaxCoursesForStudent ];
+  Course*       enrolled[MaxCoursesForStudent];
   int           numCourses; 
   
 public:
 
-	Student() :Person(), status(sUnknown), department(dUnknown) {
-	}
+	Student();
 
   // constructor:
 	Student(const string theName,
@@ -238,55 +124,29 @@ public:
 		const Date  theBirthDate,
 		const string theAddress,
 		StudentStatus theStatus,
-		Department theDepartment)
-		: Person(theName, theSSN, theBirthDate, theAddress), status(theStatus), department(theDepartment) {
-	};
+		Department theDepartment);
 
 
   // copy constructor, assignment operator, destructor:
-	 Student(const Student& other):Person(other) {
-		status = other.status;
-		department = other.department;
-	};
-	 Student& operator=(const Student& other){
-		 if (this!=&other) {
-			 Person::operator=(other);
-			 status = other.status;
-			 department = other.department;
-		 }
-		 return *this;
-	 };
-	 ~Student() {};
+	Student(const Student& other);
+	Student& operator=(const Student& other);
+	 ~Student();
 
-	 void setStatus(StudentStatus aStatus) {
-		 status = aStatus;
-	 };
-	 StudentStatus getStatus() const {
-		 return status;
-	 };
-	 void setDepartment(Department dept) {
-		 department = dept;
-	 };
-	 Department getDepartment() const {
-		 return department;
-	 };
+	 void setStatus(StudentStatus aStatus);
+	 StudentStatus getStatus() const;
+	 void setDepartment(Department dept);
+	 Department getDepartment() const;
 
-  bool enrollForCourse(const Course& aCourse);
-  bool dropFromCourse(const Course& theCourse);
-  void listCoursesRegisteredFor() const;
+	 virtual bool enrollForCourse(Course& aCourse);
+	 bool dropFromCourse(Course& theCourse);
+	 void dropAllCourse();
+	 void listCoursesRegisteredFor() const;
 
-  void print() const {
-	  Person::print();
-	  cout << "status = " << status << endl
-		  << "department = " << department << endl;
-  };
+	 virtual void print() const;
 };
 
-
-
-
-const int MaxCoursesForTeacher = 2;
-const int MaxGraders = 2;
+constexpr int MaxCoursesForTeacher = 2;
+constexpr int MaxGraders = 2;
 
 //////////// derived class Teacher /////////////
 
@@ -299,75 +159,42 @@ class Teacher : virtual public Person {
   int              numCourses;   // num of courses taught currently
   int              numGraders;
   Course*          coursesOffered[ MaxCoursesForTeacher ];
-  string*          graders[ MaxGraders ]; 
+  string           graders[ MaxGraders ]; 
   
 public:
 
-	Teacher() :Person(), rank(rUnknown), department(dUnknown) {};
+	Teacher();
 	Teacher(const string theName, long theSSN, const Date theBirthDate,
-		const string theAddress, Rank theRank, Department theDepartment)
-		: Person(theName, theSSN, theBirthDate, theAddress), rank(theRank), department(theDepartment) {
-	};
-	Teacher(const Teacher& other):Person(other) {
-		rank = other.rank;
-		department = other.department;
-	};
-	Teacher& operator=(const Teacher& other) {
-		if (this != &other) {
-			Person::operator=(other);
-			rank = other.rank;
-			department = other.department;
-		}
-		return *this;
-	};
-	~Teacher() {};
+		const string theAddress, Rank theRank, Department theDepartment, double theSalary);
+	Teacher(const Teacher& other);
+	Teacher& operator=(const Teacher& other);
+	~Teacher();
 
-	void setDepartment(Department dept) {
-		department = dept;
-	};
-	Department getDepartment() const {
-		return department;
-	};
+	void setDepartment(Department dept);
+	Department getDepartment() const;
 
   // change rank when the teacher is promoted
-	void setRank(Rank newRank) {
-		rank = newRank;
-	};
-	Rank getRank() const {
-		return rank;
-	};
-
-  void listCoursesTaught() const;
+	void setRank(Rank newRank);
+	Rank getRank() const;
+	void listCoursesTaught() const;
   // offer this course
-  bool offerCourse( const Course& aCourse );
+	bool offerCourse(Course& aCourse);
   // cancel this course
-  bool dropCourse( const Course& theCourse);
+	bool dropCourse(Course& theCourse);
+	void dropAllCourse();
 
-  bool assignGrader( const string newGrader );
-  bool dropGrader( const string grader );
-  void listGraders() const;
+	bool assignGrader(const string& newGrader);
+	bool dropGrader(const string& grader);
+	void listGraders() const;
 
-  double getSalary() const {
-	  return salary;
-  };
-  void setSalary(double theSalary) {
-	  salary = theSalary;
-  };
+	double getSalary() const;
+	void setSalary(double theSalary);
 
-  void print() const {
-	  Person::print();
-	  cout << "rank = " << rank << endl
-		  << "salary = "<< salary <<endl
-		  << "department = " << department << endl;
-  };
+	virtual void print() const;
 };
 
-
-
-
-const unsigned GradCourseLevel = 400;
-
-const unsigned MaxCoursesForGradStudent = 4;
+constexpr unsigned GradCourseLevel = 400;
+constexpr unsigned MaxCoursesForGradStudent = 4;
 
 
 ////////// derived class GraduateStudent /////////////
@@ -375,59 +202,39 @@ const unsigned MaxCoursesForGradStudent = 4;
 
 class GraduateStudent : public Student {
   
-  Teacher advisor;
-  unsigned short numCourses;
+  Teacher* advisor;
+  unsigned short numCourses; 
 
 public:
   
   //constructor:
-	GraduateStudent() :Student() ,advisor(){};
-	GraduateStudent(const string theName,
-			unsigned long theSSN,
-			const Date theBirthDate,
-			const string theAddress,
-			StudentStatus theStatus,
-			Department theDepartment,
-			const Teacher& theAdvisor)
-			: Student(theName, theSSN, theBirthDate, theAddress, theStatus, theDepartment), advisor(theAdvisor) {};
-  
-	GraduateStudent(const GraduateStudent& other): Student(other){
-		advisor = other.advisor;
-	};
-	GraduateStudent&  operator=(const GraduateStudent& other) {
-		if (this != &other) {
-			Student::operator=(other);
-			advisor = other.advisor;
-		}
-		return *this;
-	};
-  ~GraduateStudent(){}
+	GraduateStudent();
+	GraduateStudent(const string& theName,
+		unsigned long theSSN,
+		const Date theBirthDate,
+		const string& theAddress,
+		StudentStatus theStatus,
+		Department theDepartment,
+		Teacher& theAdvisor);
+	GraduateStudent(const GraduateStudent& other);
+	GraduateStudent&  operator=(const GraduateStudent& other);
+	~GraduateStudent();
+	//need to override the following method inherited from 
+	//Student class because a grad students cannot enroll in 
+	//low-level courses
+	bool enrollForCourse(Course& aCourse );
 
+	void changeAdvisor(Teacher& newAdvisor);
 
-  //need to override the following method inherited from 
-  //Student class because a grad students cannot enroll in 
-  //low-level courses
-  bool enrollForCourse( const Course& aCourse );
-
-  void changeAdvisor(const Teacher& newAdvisor) {
-	  advisor = newAdvisor;
-  };
-
-  Teacher getAdvisor() const {
-	  return advisor;
-  };
-
-  void print() const {
-	  Student::print();
-	  cout << "advisor = " << advisor.getName() << endl;
-  };
+	Teacher& getAdvisor() const;
+	virtual void print() const;
 };
 
 
 
 //////////// derived class GradTeachAsst ///////////////
 
-
+/*
 class GradTeachAsst : public GraduateStudent, public Teacher {
 
 public:
@@ -463,3 +270,4 @@ public:
   // from two different bases  
   void print() const;   
 };
+*/
